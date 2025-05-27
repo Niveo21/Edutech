6969168aeb86090a9528934318928189ac8b367f
Niveo21 <140994318+Niveo21@users.noreply.github.com>
2025-05-27 17:22:27 -0400

    Delete Edutech directory

--- Edutech/src/main/java/com/example/Edutech/repository/PersonaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Edutech.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.Edutech.model.Persona;
-
-public interface PersonaRepository extends JpaRepository <Persona, String>{
-
-}
